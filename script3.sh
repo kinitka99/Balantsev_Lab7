8aab8f21c7f4b5e50744075bdc10e7f3ef08c848
Kinitka99 <n.balantsev@gmail.com>
2025-01-04 23:27:28 +0300

    Created script3.sh

---
+++ script3.sh
@@ -0,0 +1,9 @@
+read -p "Enter the path to archivate: " dirpath
+
+if [ -d "$dirpath" ]; then
+	current_date=$(date +%Y-%m-%d)
+	archive_name="$(basename "$dirpath")_$current_date.tar.gz"
+	tar -czf "$archive_name" -C "$(dirname "$dirpath")" "$(basename "$dirpath")"
+else
+	echo "The path dsnt exists..."
+fi
